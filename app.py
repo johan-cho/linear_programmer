d219aaaf0bc8eeb83a6ee3e71d90853198972d71
Johan Cho <cho.joh@northeastern.edu>
2023-09-20 13:42:11 -0400

    move from main.py

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 """Run the application."""
+import logging
 from flask import Flask, request, render_template
 from helper_functions import gen_solver, solve
 
 
-def create_app():
+def test_solve() -> str:
+    """Test the solve function"""
+    objective_func = {"maximize": "50*x1 + 20*x2 + 25*x3"}
+
+    solvah = gen_solver(
+        objective_func,
+        [
+            "9*x1 + 3*x2 + 5*x3 <= 500",
+            "5*x1 + 4*x2 <= 350",
+            "3*x1 + 2*x3 <= 150",
+            "x1 >= 0",
+            "x2 >= 0",
+            "0 <= x3 <= 20",
+        ],
+        epsilon=0.7,
+    )
+    return solve(solvah, "down", next(iter(objective_func.values())))
+
+
+def create_app() -> Flask:
     """Create a Flask application"""
     __app = Flask(__name__)
 
@@ -32,6 +52,9 @@ def create_app():
     return __app
 
 
+app = create_app()
+
+
 if __name__ == "__main__":
-    app = create_app()
+    logging.getLogger().setLevel(logging.INFO)
     app.run(debug=True)
